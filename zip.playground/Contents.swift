af329b5ebe1f340ed6372c0174adeba062b7fbac
wanhar <wanhar@thelionparcel.com>
2021-12-12 18:51:05 +0800

    zip

---
+++ zip.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let students = ["Alex", "Mary", "John", "Steven", "Maro"]
+let grades = [3.4, 2.8, 3, 4]
+
+let pair = zip(students, grades)
+
+for studentAndGrade in pair {
+    print(studentAndGrade.0)
+    print(studentAndGrade.1)
+}
+
+// student maro will not printd, because not have a pair grade
