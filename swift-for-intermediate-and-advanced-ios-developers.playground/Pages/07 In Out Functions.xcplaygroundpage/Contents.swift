ea228c5e10f9eeb9a6e5ca79bb3bd0e598c1d872
wanhar <wanhar@thelionparcel.com>
2021-12-12 21:47:34 +0800

    restructure folder

---
+++ swift-for-intermediate-and-advanced-ios-developers.playground/Pages/07 In Out Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+//: [Previous](@previous)
+
+import UIKit
+
+// In Out Parameters
+struct User {
+    var userId: Int?
+    let name: String
+}
+
+func saveUser(_ user: inout User) {
+    user.userId = 800
+}
+
+var user = User(name: "Wanhar")
+saveUser(&user)
+
+print(user)
+
+//: [Next](@next)
