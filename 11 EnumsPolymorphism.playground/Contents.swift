14193c3e49b36d5112193f70c38f302a4f5126bc
wanhar <wanhar@thelionparcel.com>
2021-12-14 22:05:40 +0800

    Hiding type using enums

---
+++ 11 EnumsPolymorphism.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+struct Teacher {
+    let name: String
+    let courses: [String]
+}
+
+struct Student {
+    let name: String
+    let courses: [String]
+    var grade: String?
+}
+
+let teacher = Teacher(name: "Wanhar", courses: ["Language", "English"])
+let student = Student(name: "Daeng Maro", courses: ["Language", "English"])
+
+let users: [Any] = [teacher, student]
+
+for user in users {
+    switch user {
+        case let user as Student:
+            print(user.grade ?? "")
+        case let user as Teacher:
+            print(user.courses)
+        default:
+            print("Not student or teacher")
+    }
+}
+
+enum User {
+    case teacher(Teacher)
+    case student(Student)
+}
+
+let allUsers = [User.teacher(teacher), User.student(student)]
+
+for user in allUsers {
+    switch user {
+        case .student(let student):
+            print(student.grade ?? "")
+        case .teacher(let teacher):
+            print(teacher.courses)
+    }
+}
