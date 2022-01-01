ef954c92fc49048781d5fd4e58deae25d9e50a9e
wanhar <wanhardaengmaro@gmail.com>
2022-01-01 22:05:37 +0800

    initializers

---
+++ 17 StructInitializers.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+struct Student {
+    let firstName: String
+    let lastName: String
+    let grade: String
+    
+}
+
+extension Student {
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.grade = ""
+    }
+}
